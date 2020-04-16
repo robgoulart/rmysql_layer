df43cc53502be3a0132994d611dcacfe89394f2b
Roberto Goulart <robertogoulart@outlook.com>
2020-04-16 09:45:11 -0300

    Second commit. After update of connections.json and README.md

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
-# Data access layer to MySQL using R for multi proposal
+# R data access layer to MySQL using multi proposal
+
+Libraries required:
+- RMySQL
+- stringr
+- rjson
 
 Methods:
-- connect. return an RMySql connection object
-- insert. insert data. Receive 2 or 3 arguments. Fields is optional, SQL accept INSERT query with valores, without fields and MYSQL takes care of insert in the query order.
-- select. select data
+
+- connect. Return a RMySql connection object. The connections must be in a JSON file in this format:
+	&lt;p&gt;{
+ 		&lt;p&gt;"connection1":
+			&lt;p&gt;{
+				&lt;p&gt;"user" : "user1",
+				&lt;p&gt;"password" : "passwd1",
+				"&lt;p&gt;host": "host1",
+				&lt;p&gt;"database" : "db1"
+			&lt;p&gt;},
+ 		&lt;p&gt;"connection2":
+ 			&lt;p&gt;{
+ 				&lt;p&gt;"user" : "user2",
+				&lt;p&gt;"password" : "passwd2",
+				&lt;p&gt;"host" : "host2",
+				&lt;p&gt;"database" : "db2"
+			&lt;p&gt;}
+	&lt;p&gt;}
+
+- insert. Insert data in a table. Receive 2 or 3 arguments. Specify arguments is mandatory. Values with ' are replaced with @.
+	&lt;1) table where data will be inserted: character
+	&lt;2) fields in table (optional, SQL accept INSERT query with valores, without fields and MYSQL takes care of insert in the query order)
+	&lt;3) values inserted in table 
+
+- select. Select data
--- connection_mysql.R
+++ connection_mysql.R
@@ -1,52 +1,46 @@
 library("RMySQL")
 library("stringr")
+library("rjson")
 
-conecta=function(){
+connect=function(name){
+  dados_conn=fromJSON(file='connections.json')
+  dados_conn=unlist(dados_conn[name])
   con=dbConnect(MySQL(),
-                user='u303739996_localidade',
-                password='canetaazul',
-                host='92.249.44.1',
-                dbname='u303739996_localidade')
+                user=dados_conn[1],
+                password=dados_conn[2],
+                host=dados_conn[3],
+                dbname=dados_conn[4])
   return(con)
 }
 
-# função que executa uma query INSERT. Pode passar 2 ou 3 argumentos. O argumento 'campos' é opcional, pois
-# a sintaxe SQL aceita uma query INSERT apenas com valores e o MYSQL se encarrega de inserir na ordem que está na query
-
-# ATENÇÃO: é obrigatório especificar os nomes dos argumento na chamada da função pois existe um argumento opcional (campos)
-
-# 1) a tabela onde serão inseridos os dados: formato character (string)
-# 2) campos na tabela
-# 3) valores que serão inseridos na tabela, são referentes aos campos
-
-insere_registro=function(conexao, tabela, campos=c(), valores, print=FALSE){
-  valores_t=c()
-  for (v in valores){
-    valores_t=paste(valores_t, paste("'",str_replace(v,"'" ,"@"), "',", sep=""), sep="")
+insert=function(connection, table, fields=c(), values, print=FALSE){
+  values_t=c()
+  for (v in values){
+    values_t=paste0(values_t, paste0("'",str_replace(v,"'" ,"@"), "',"))
   }
-  valores_t=substr(valores_t, 1, nchar(valores_t)-1)
-  if (length(campos)>0){
-    campos_t=c()
-    for (c in campos){
-      campos_t=paste(campos_t, paste("",c, ",", sep=""), sep="")
+  values_t=substr(values_t, 1, nchar(values_t)-1)
+  if (length(fields)>0){
+    fields_t=c()
+    for (c in fields){
+      fields_t=paste0(fields_t, paste0("",c, ","))
     }
-    campos_t=substr(campos_t, 1, nchar(campos_t)-1)
-    query=paste("INSERT INTO ", tabela, " (", campos_t, ")", " VALUES (", valores_t, ");", sep="")
+    fields_t=substr(fields_t, 1, nchar(fields_t)-1)
+    query=paste0("INSERT INTO ", table, " (", fields_t, ")", " VALUES (", values_t, ");")
   } else {
-    query=paste("INSERT INTO ", tabela, " VALUES (", valores_t, ");", sep="")
+    query=paste0("INSERT INTO ", table, " VALUES (", values_t, ");")
   }
   if (print==TRUE){
     print(query)
   }
-  res=dbGetQuery(conexao, query)
+  res=dbGetQuery(connection, query)
   return(res)
 }
 
-seleciona=function(conexao, query){
-  res=dbGetQuery(conexao, query)
+select=function(connection, query){
+  res=dbGetQuery(connection, query)
   return(res)
 }
 
-fecha_conexao=function(con){
-  dbDisconnect(con)
+close=function(connection){
+  dbDisconnect(connection)
 }
